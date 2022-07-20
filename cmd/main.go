88d6845d8616cd6fd3cf006f7fefe7260a7a93fb
满仓 <hejinxue001@lonlife.cn>
2022-07-20 16:26:24 +0800

    zerolog 分级

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"github.com/flyflyhe/dbMigrate/internal/db"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"io/ioutil"
-	"log"
+	"strconv"
 	"sync"
 )
 
@@ -23,26 +24,25 @@ func main() {
 				zerolog.SetGlobalLevel(zerolog.DebugLevel)
 			}
 			if yamlFileBytes, err := ioutil.ReadFile(yamlFile); err != nil {
-				log.Println(err)
+				log.Error().Caller().Err(err).Send()
 				return
 			} else {
 				if taskExternalConfigList, err := db.GetConfig(yamlFileBytes); err != nil {
-					log.Println(err)
 					return
 				} else {
-					log.Println(taskExternalConfigList)
-
 					wg := sync.WaitGroup{}
 					wg.Add(len(taskExternalConfigList))
 					for k, v := range taskExternalConfigList {
-						log.Println("任务", k, "--", v.T0, "===>", v.T1)
+						log.Info().Caller().Str("", "任务"+strconv.Itoa(k)+"--"+v.T0+"===>"+v.T1).Send()
 						task := db.CreateTask(v.Dsn0, v.Dsn1, v.T0, v.T1, v.DsnType0, v.DsnType1)
 						taskConfig := db.CreateTaskConfigByEConfig(v)
 
 						go func() {
 							defer wg.Done()
 							task.SetFuncByConfig(taskConfig)
-							log.Println(task.Migrate())
+							if err := task.Migrate(); err != nil {
+								log.Error().Caller().Err(err).Send()
+							}
 						}()
 					}
 					wg.Wait()
@@ -55,6 +55,6 @@ func main() {
 	command.Flags().BoolVarP(&debug, "debug", "d", false, "是否开启debug")
 	_ = command.MarkFlagRequired("yaml")
 	if err := command.Execute(); err != nil {
-		log.Println(err)
+		log.Error().Caller().Err(err).Send()
 	}
 }
