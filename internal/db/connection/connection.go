3e8be7453fc6b2dedb7c60763d78df05a0c9d938
hejinxue <hjx@hnfesco.com>
2023-05-04 09:57:22 +0800

    自定义logger

--- internal/db/connection/connection.go
+++ internal/db/connection/connection.go
@@ -5,6 +5,10 @@ import (
 	"github.com/dbMigrate/v2/config"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 )
 
 func InitDb(config config.MysqlConfig) (*gorm.DB, error) {
@@ -12,6 +16,12 @@ func InitDb(config config.MysqlConfig) (*gorm.DB, error) {
 		config.User, config.Password, config.Host, config.Port, config.Database,
 	) + "&loc=Asia%2fShanghai"
 	fmt.Println("dsn:" + dsn)
+	gormLogger := logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+		SlowThreshold:             400 * time.Millisecond,
+		LogLevel:                  logger.Warn,
+		IgnoreRecordNotFoundError: false,
+		Colorful:                  true,
+	})
 	if db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:                       dsn,   // DSN data source name
 		DefaultStringSize:         256,   // string 类型字段的默认长度
@@ -19,7 +29,7 @@ func InitDb(config config.MysqlConfig) (*gorm.DB, error) {
 		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
 		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
 		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
-	}), &gorm.Config{}); err != nil {
+	}), &gorm.Config{Logger: gormLogger}); err != nil {
 		return nil, err
 	} else {
 		if sqlDb, err := db.DB(); err != nil {
